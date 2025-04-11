871968467870fe65bd2a79e73b67e917caea2e35
2faced-sword <murtazakolachi10@gmail.com>
2025-04-12 04:25:23 +0500

    Update evolution.sh

--- evolution.sh
+++ evolution.sh
@@ -5,7 +5,7 @@ git clone https://github.com/2faced-sword/android_device_xiaomi_apollon -b vic d
 git clone https://github.com/2faced-sword/android_device_xiaomi_sm8250-common -b lineage-22.2 device/xiaomi/sm8250-common && \
 git clone https://github.com/2faced-sword/proprietary_vendor_xiaomi_apollon -b lineage-22.2 vendor/xiaomi/apollon && \
 git clone https://github.com/2faced-sword/proprietary_vendor_xiaomi_sm8250-common -b lineage-22.2 vendor/xiaomi/sm8250-common && \
-git clone https://github.com/2faced-sword/android_kernel_xiaomi_sm8250 -b lineage-22.2 kernel/xiaomi/apollon && \
+git clone https://github.com/2faced-sword/android_kernel_xiaomi_sm8250 -b lineage-22.2 kernel/xiaomi/sm8250-common && \
 git clone https://github.com/2faced-sword/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
 /opt/crave/resync.sh; \
 export BUILD_USERNAME=Krishy24; \
