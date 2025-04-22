05f2a24b8c59193103303f211e9e1d6d484111b7
2faced-sword <murtazakolachi10@gmail.com>
2025-04-23 03:56:17 +0500

    crdroid 11.4

--- crdroid.sh
+++ crdroid.sh
@@ -1,8 +1,8 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/2faced-sword/build_manifest -b crdroid .repo/local_manifests && \
 /opt/crave/resync.sh && \
+git clone https://github.com/2faced-sword/build_manifest -b crdroid .repo/local_manifests && \
 export BUILD_USERNAME=Krishy; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/New Delhi; \
