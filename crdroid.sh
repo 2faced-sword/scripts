d97002c5c060b8fb2a2f5636d6a18e8d28eaf8b2
2faced-sword <murtazakolachi10@gmail.com>
2025-04-22 23:06:05 +0500

    crdroid 11.4

--- crdroid.sh
+++ crdroid.sh
@@ -1,14 +1,11 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
-/opt/crave/resync.sh; \
-git clone https://github.com/2faced-sword/android_device_xiaomi_apollo -b crdroid device/xiaomi/apollo && \
-git clone https://github.com/2faced-sword/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/2faced-sword/kernel_xiaomi_apollo -b main kernel/xiaomi/common-sm8250 && \
-git clone https://github.com/2faced-sword/hardware_xiaomi -b fifteen hardware/xiaomi && \
+git clone https://raw.githubusercontent.com/2faced-sword/build_manifest/refs/heads/crdroid/local_manifest.xml -b crdroid .repo/local_manifests && \
+/opt/crave/resync.sh && \
 export BUILD_USERNAME=Krishy; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/New Delhi; \
-source build/envsetup.sh; \
-breakfast apollo user; \
+source build/envsetup.sh && \
+breakfast apollo user && \
 mka bacon
